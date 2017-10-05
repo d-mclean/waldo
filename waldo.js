9ba603d1e952593d0476772ffd26757393a9e4ef
Darcy McLean <darc.mclean@gmail.com>
2017-10-05 15:11:09 +0000

    Prints the index where waldo is found.

--- waldo.js
+++ waldo.js
@@ -1,14 +1,18 @@
+/* 20171005 DM - LHL w1d4
+
+  This js file searches an array for Waldo and displays the index.
+*/
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
-      found();   // execute callback
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
-  console.log("Found him!");
+function actionWhenFound(indexFound) {
+  console.log("Found Waldo at index " + indexFound + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
